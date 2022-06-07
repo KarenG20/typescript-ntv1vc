73f2f63e1f5b62c26e694884738e566b8f1a1145
KarenG20 <karen.karenromero.gomez@gmail.com>
2022-06-07 12:49:42 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-ntv1vc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-ntv1vc)
---
+++ index.ts
@@ -0,0 +1,8 @@
+console.log('Hello world');
+
+const a: number = 10;
+
+console.log(a);
+
+// IMPORTANTE:
+// Ver la consola de JavaScript
